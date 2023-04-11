53a2d3106b264f2f091d31ebe61f6283ed754489
achraffawzi <achraffawzi224a@gmail.com>
2023-04-11 16:25:57 +0000

    Creating & designing reset password page

--- pages/auth/reset-password.vue
+++ pages/auth/reset-password.vue
@@ -1,9 +1,31 @@
 <template>
     <div>
-reset password
+        <Alert @on-click="handleCloseAlert" v-if="isAlertOpen" :content="alert!.content" :icon-class="alert!.iconClass" :dark="alert!.dark" :light="alert!.light" />
+        <FormInput type="password" label="New Password" name="new password" :value="newPassword" />
+        <FormInput type="password" label="Confirm new Password" name="new password confirm" :value="newPasswordConfirm" class="mb-3"/>
+        <Button content="Reset" :full="true" @click="andleResetPassword" />
     </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import {ref} from 'vue'
+import { useAuth, useAlert } from '~/composables';
+definePageMeta({
+    layout: 'auth'
+})
 
+const { alert, setAlert, isAlertOpen, clearAlert } = useAlert()
+const newPassword = ref<string>('')
+const newPasswordConfirm = ref<string>('')
+
+const andleResetPassword = () => {
+    if(!newPassword.value || !newPasswordConfirm.value) {
+        setAlert('Both passwords are required', 'ERROR');
+        return;
+    }
+}
+
+const handleCloseAlert =() => {
+    clearAlert()
+}
 </script>
