2e79f636171cd888528d2057af250a93d32c733b
achraffawzi <achraffawzi224a@gmail.com>
2023-04-11 13:52:16 +0000

    making setAlert more reusable using switch case

--- composables/useAlert.ts
+++ composables/useAlert.ts
@@ -1,20 +1,66 @@
 import { ref, onBeforeMount } from "vue";
-import { IAlert } from "~/types";
+import { IAlert, AlertTypes } from "~/types";
 
 export const useAlert = () => {
+  const config = useRuntimeConfig();
   let alert = ref<IAlert | null>(null);
   let isAlertOpen = ref<boolean>(false);
 
-  const setAlert = (payload: IAlert | null): void => {
-    alert.value = { ...payload } as IAlert | null;
-    isAlertOpen.value = !!payload;
+  const setAlert = (content: string, type: AlertTypes): void => {
+    switch (type) {
+      case "ERROR":
+        alert.value = {
+          content,
+          iconClass: config.public.ICONS.ERROR,
+          dark: config.public.COLORS.ALERT_DARK_ERROR,
+          light: config.public.COLORS.ALERT_LIGHT_ERROR,
+        };
+        break;
+
+      case "SUCCESS":
+        alert.value = {
+          content,
+          iconClass: config.public.ICONS.SUCCESS,
+          dark: config.public.COLORS.ALERT_DARK_SUCCESS,
+          light: config.public.COLORS.ALERT_LIGHT_SUCCESS,
+        };
+        break;
+
+      case "WARNING":
+        alert.value = {
+          content,
+          iconClass: config.public.ICONS.WARNING,
+          dark: config.public.COLORS.ALERT_DARK_WARNING,
+          light: config.public.COLORS.ALERT_LIGHT_WARNING,
+        };
+        break;
+
+      case "INFO":
+        alert.value = {
+          content,
+          iconClass: config.public.ICONS.INFO,
+          dark: config.public.COLORS.ALERT_DARK_INFO,
+          light: config.public.COLORS.ALERT_LIGHT_INFO,
+        };
+        break;
+    }
+
+    isAlertOpen.value = true;
   };
 
-  onBeforeMount(() => setAlert(null));
+  const clearAlert = () => {
+    alert.value = null;
+    isAlertOpen.value = false;
+  };
+
+  onBeforeMount(() => {
+    clearAlert();
+  });
 
   return {
     alert,
     isAlertOpen,
     setAlert,
+    clearAlert,
   };
 };
