adb69e6f590aa8f61e26fbff36f24235095f9346
achraffawzi <achraffawzi224a@gmail.com>
2023-04-17 10:48:54 +0000

    Creating dislike schema

---
+++ server/db/models/dislikes.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const dislikeSchema = new mongoose.Schema(
+  {
+    postId: {
+      type: mongoose.Schema.Types.ObjectID,
+      ref: "Post",
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectID,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("dislike", dislikeSchema);
