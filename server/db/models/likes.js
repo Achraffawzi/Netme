abd9e49b7f28b19cfeb9a4475db1fd3756203880
achraffawzi <achraffawzi224a@gmail.com>
2023-04-17 10:48:03 +0000

    Creating likes schema

---
+++ server/db/models/likes.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const likeSchema = new mongoose.Schema(
+  {
+    postId: {
+      type: mongoose.Schema.Types.ObjectID,
+      ref: "Post",
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectID,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("like", likeSchema);
