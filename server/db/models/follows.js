29929cb7c0e47ad66ab4e05c0bf5e12af8d251a0
achraffawzi <achraffawzi224a@gmail.com>
2023-04-17 10:47:02 +0000

    Creating follow schema

---
+++ server/db/models/follows.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const followSchema = new mongoose.Schema(
+  {
+    followerId: {
+      type: mongoose.Schema.Types.ObjectID,
+      ref: "User",
+    },
+    followingId: {
+      type: mongoose.Schema.Types.ObjectID,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("follow", followSchema);
