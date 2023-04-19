fed3e49d47f882870f08212556c4fa0e5c4a0b66
achraffawzi <achraffawzi224a@gmail.com>
2023-04-19 12:07:01 +0000

    recreating env vars to the project

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 
 export default async (_nitroApp: Nitro) => {
   const config = useRuntimeConfig();
+  console.log(config?.MONGO_URI);
 
   try {
     await mongoose.connect(config?.MONGO_URI);
