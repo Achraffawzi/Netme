267cdaf0a15d92d56bd5969670946bb6f0cca56d
achraffawzi <achraffawzi224a@gmail.com>
2023-04-20 14:42:21 +0000

    Creating auth middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,28 @@
+import { decodeToken } from "~/server/utils/jwt.js";
+import URLPattern from "url-pattern";
+
+export default defineEventHandler((event) => {
+  // endpoints to be handled by this middleware
+  const ENDPOINTS = ["/api/auth/logout", "/api/users/user", "/api/posts/user"];
+
+  // check if upcoming req is handled by this middleware
+  const isHandledByAuthMiddleware = ENDPOINTS.some((endpoint) => {
+    const pattern = new URLPattern(endpoint);
+    return pattern.match(event.node.req.url);
+  });
+
+  if (!isHandledByAuthMiddleware) return;
+
+  const { access_token } = parseCookies(event);
+
+  if (!access_token) {
+    return sendError(event, {
+      statusCode: 403,
+      statusMessage: "You're unauthorized",
+    });
+  }
+
+  // decode access_token
+  const { _id } = decodeToken(access_token);
+  event.context._id = _id;
+});
