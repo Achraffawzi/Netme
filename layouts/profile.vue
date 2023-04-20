aa999ed5a52725e4ffbce9f78a2e2ca400ae296e
achraffawzi <achraffawzi224a@gmail.com>
2023-04-20 14:17:29 +0000

    Creating profile layout

---
+++ layouts/profile.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="bg-creemy  min-h-screen flex flex-col">
+        <Navbar />
+        <div class="flex flex-col flex-1 pt-16 items-center ">
+            <slot></slot>
+        </div>
+    </div>
+</template>
