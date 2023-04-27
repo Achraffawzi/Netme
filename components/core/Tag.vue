4f437c47f93d7464fc2c8125dbdac1f35bb55947
achraffawzi <achraffawzi224a@gmail.com>
2023-04-27 13:36:43 +0100

    Creating tag component

---
+++ components/core/Tag.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="py-1 px-2  bg-gray-200 rounded-3xl text-xs">
+        {{ content }}
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    content: {
+        type: String,
+        required: true
+    }
+})
+</script>
