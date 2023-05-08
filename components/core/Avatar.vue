96694bf1a16b42dd89633895cab01705a8003889
achraffawzi <achraffawzi224a@gmail.com>
2023-05-08 12:00:47 +0100

    Making Avatar uses default no image placeholder

--- components/core/Avatar.vue
+++ components/core/Avatar.vue
@@ -1,12 +1,15 @@
 <template>
     <div class="rounded-full">
-        <img class="rounded-full" :style="getAvatarSize" :src="src" :alt="alt || 'Avatar'" loading="lazy">
+        <img class="rounded-full" :style="getAvatarSize" :src="src ?? NoPicPlaceholder" :alt="alt || 'Avatar'" loading="lazy">
     </div>
 </template>
 
 <script setup lang="ts">
-import {computed} from 'vue'
-const {size} = defineProps({
+import {computed} from 'vue';
+import NoPicPlaceholder from '~/assets/images/noPicPlaceholder.jpg';
+
+
+const {size, src} = defineProps({
     src: {
         type: String,
         required: true,
@@ -18,6 +21,8 @@ const {size} = defineProps({
         type: Number,
     }
 })
+
+// const setAvatarImage = computed(() => src ?? import.meta.glob('~/assets/images/noPicPlaceholder.jpg'))
 
 const getAvatarSize = computed(() => ({
     width: `${size}px`,
