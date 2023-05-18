622b01690d7543f54bd11461776b37d05a6401cd
achraffawzi <achraffawzi224a@gmail.com>
2023-05-18 20:26:55 +0100

    Create Tags compo that takes list the list of tags

---
+++ components/Tags.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex items-center gap-x-2">
+        <Tag v-for="(item, index) in tags" :key="index" :content="item" />
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    tags: {
+        type: Array,
+        required: true,
+    }
+})
+</script>
